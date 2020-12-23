1d0e523c572ce714fd77dd61de1efd02234c594f
zaidalasfar97 <z.alasfar@hotmail.com>
2020-12-23 16:14:48 +0200

    done

---
+++ java.js
@@ -0,0 +1,24 @@
+var username='Guest'
+username= prompt('Please enter your name ');
+var major= prompt('Please write your major');
+var gpa= prompt('Enter your GPA')
+
+
+
+alert('Hello '+username+' here');
+alert('Your major is '+major+' ');
+
+if  (gpa>50){
+    document.write('<p>you are pass</p>')
+}else {
+    document.write('<p>you are Fail</p>')
+}
+
+
+document.write('Hello '+username+' here');
+
+document.write('Your major is '+major+' ');
+
+
+
+
