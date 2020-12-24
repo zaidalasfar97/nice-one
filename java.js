ff00781333d9d2c5112cb101b39af1b6c3e0d5ce
zaidalasfar97 <z.alasfar@hotmail.com>
2020-12-24 16:03:55 +0200

    done

--- java.js
+++ java.js
@@ -3,10 +3,30 @@ username= prompt('Please enter your name ');
 var major= prompt('Please write your major');
 var gpa= prompt('Enter your GPA')
 
+while(username === ''||  username ===  null) {
+
+
+    username =  prompt ('Enter your name ')
+    
+}
+
+while (major === ''|| major === null){
+
+    major= prompt('Enter your major')
+}
 
 
 alert('Hello '+username+' here');
 alert('Your major is '+major+' ');
+
+var number = prompt('How  many experience years do you have');
+
+for (var i = 1 ; i <= number ; i++){
+
+    document.write('<img src="/images/chemical.jpg" alt="Picture"/>')
+
+}
+
 
 if  (gpa>50){
     document.write('<p>you are pass</p>')
@@ -17,7 +37,8 @@ if  (gpa>50){
 
 document.write('Hello '+username+' here');
 
-document.write('Your major is '+major+' ');
+
+
 
 
 
