14a83e468a2172d2b97e2b22bc33df8ea7c5d2fd
zaidalasfar97 <z.alasfar@hotmail.com>
2020-12-27 16:20:46 +0200

    done

--- java.js
+++ java.js
@@ -1,41 +1,70 @@
-var username='Guest'
-username= prompt('Please enter your name ');
-var major= prompt('Please write your major');
-var gpa= prompt('Enter your GPA')
+// var username='Guest'
+// username= prompt('Please enter your name ');
+// var major= prompt('Please write your major');
+// var gpa= prompt('Enter your GPA')
 
-while(username === ''||  username ===  null) {
+// while(username === ''||  username ===  null) {
 
 
-    username =  prompt ('Enter your name ')
+//     username =  prompt ('Enter your name ')
     
+// }
+
+// while (major === ''|| major === null){
+
+//     major= prompt('Enter your major')
+// }
+
+
+// alert('Hello '+username+' here');
+// alert('Your major is '+major+' ');
+
+// var number = prompt('How  many experience years do you have');
+
+// for (var i = 1 ; i <= number ; i++){
+
+//     document.write('<img src="/images/chemical.jpg" alt="Picture"/>');
+//     document.write(i);
+// }
+
+
+// if  (gpa>50){
+//     document.write('<p>you are pass</p>')
+// }else {
+//     document.write('<p>you are Fail</p>')
+// }
+
+
+// document.write('Hello '+username+' here');
+
+var username=getInformation(username,'Please insert your name ');
+
+var major=getInformation(major,'Please enter your major');
+
+var age=getInformation(age,'Please insert your age ');
+
+displayMessage(username,major,age)
+
+
+function takemessage(message){
+    return prompt(message);
 }
 
-while (major === ''|| major === null){
+function displayMessage(uName,Umajor,Uage){
 
-    major= prompt('Enter your major')
+ alert ('Hello' + uName + '.Your major is ' + Umajor + ' .Your age is ' + Uage );
+
 }
 
+function getInformation(userInput, message){
+    while (userInput === undefined || userInput=== '' || userInput=== null){
+    userInput=takemessage(message);}
 
-alert('Hello '+username+' here');
-alert('Your major is '+major+' ');
+    return userInput
 
-var number = prompt('How  many experience years do you have');
 
-for (var i = 1 ; i <= number ; i++){
 
-    document.write('<img src="/images/chemical.jpg" alt="Picture"/>')
-    document.write(i)
 }
-
-
-if  (gpa>50){
-    document.write('<p>you are pass</p>')
-}else {
-    document.write('<p>you are Fail</p>')
-}
-
-
-document.write('Hello '+username+' here');
 
 
 
